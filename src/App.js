3df101579c410bb2d592b661e75c234d0c6b919a
Benoit Dekajoo <dekajoo@gmail.com>
2022-03-14 10:15:00 +0100

    yo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import recettes from './Recettes.js';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState({});
+  const [nameNewItem, setNameNewItem] = useState("");
+
+  function addItems(recetteName) {
+    var newItems = Object.assign({}, items);
+
+    let recette = recettes.find(x => x.name === recetteName);
+
+    for (var item of Object.entries(recette.ingredients)) {
+      if (!newItems[item[0]])
+        newItems[item[0]] = item[1];
+      else
+        newItems[item[0]] += item[1];
+    }
+    console.log("aadd", newItems)
+
+    setItems(newItems);
+  }
+
+  function decreaseItem(itemName) {
+    var newItems = Object.assign({}, items);
+    if (newItems[itemName] == 0)
+      return;
+    newItems[itemName]--;
+    setItems(newItems);
+  }
+
+  function increaseItem(itemName) {
+    var newItems = Object.assign({}, items);
+    newItems[itemName]++;
+    setItems(newItems);
+  }
+
+  function createNewItem() {
+    if (!nameNewItem)
+      return;
+
+    var newItems = Object.assign({}, items);
+    if (!newItems[nameNewItem])
+      newItems[nameNewItem] = 1;
+    else
+      newItems[nameNewItem] += 1;
+
+    setItems(newItems);
+    setNameNewItem("");
+  }
+
+  function copyToClipBoard() {
+    var text = "";
+    for (var item of Object.entries(items)) {
+      text += item[0] + " x" + item[1] + "\n"
+    };
+    navigator.clipboard.writeText(text);
+  }
+
+  function reset() {
+    setItems({});
+  }
+
+  function exportToGKeep() {
+    copyToClipBoard();
+    setTimeout(() => {
+      window.open("http://keep.new");
+    }, 500)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="title">
+          Koors
+        </div>
+
+        <div className="sub-title">
+          Recettes
+        </div>
+        <div className="recettes">
+          {recettes.map((recette, i) => <div key={i} className={"recette" + (recette.singleItem ? " singleItem" : "")} onClick={() => addItems(recette.name)}>
+            <div className="recette-title">{recette.name}</div>
+          </div>)}
+        </div>
+
+        <div className="sub-title">
+          Courses
+        </div>
+        <div className="list">
+          {Object.entries(items).map((item, i) => <div key={i} className="item">
+            <span className="itemName">{item[0]}</span>
+            <span className="itemButton" onClick={() => decreaseItem(item[0])}>⬅</span>
+            <span className="itemQuantity">{item[1]}</span>
+            <span className="itemButton" onClick={() => increaseItem(item[0])}>➡</span></div>)}
+          <div className="item">
+            <input type="text" className="itemNameField" onChange={e => setNameNewItem(e.target.value)} value={nameNewItem}></input>
+            <button className="actionButton" disabled={!nameNewItem} onClick={() => createNewItem()}>Add</button>
+          </div>
+        </div>
+
+        <div className="actions">
+          <button className="actionButton" onClick={() => reset()}>reset</button>
+          <button className="actionButton" onClick={() => copyToClipBoard()}>copy</button>
+          <button className="actionButton" onClick={() => exportToGKeep()}>export to google keep</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Recettes.js
@@ -0,0 +1,74 @@
+let recettes = [{
+  name: "Tarte tatin aux tomates",
+  ingredients: {
+    "tomate": 7,
+    "pâte feuilletée": 1,
+    "pignons de pain": 1
+  }
+},
+{
+  name: "Stew",
+  ingredients: {
+    "oignon": 1,
+    "ail": 5,
+    "gingembre": 1,
+    "curcuma": 1,
+    "pois chiches": 2,
+    "Lait de coco gras 400ml": 1,
+    "bouillons cube": 1,
+    "epinards frais": 1,
+    "pot yaourt Skir": 1,
+    "tomate": 2,
+    "echalottes": 2
+  }
+},
+{
+  name: "Burger végé féta",
+  ingredients: {
+    "féta": 1,
+    "oeuf": 1,
+    "farine": 1,
+    "graines": 1,
+    "cumin": 1,
+    "pain à burger": 2,
+    "cornichons": 1,
+    "ketchup": 1
+  }
+},
+{
+  name: "PQ",
+  singleItem: true,
+  ingredients: {
+    "PQ": 1,
+  }
+},
+{
+  name: "café",
+  singleItem: true,
+  ingredients: {
+    "café": 1,
+  }
+},
+{
+  name: "dentrifice",
+  singleItem: true,
+  ingredients: {
+    "dentrifice": 1,
+  }
+},
+{
+  name: "huile d'olive",
+  singleItem: true,
+  ingredients: {
+    "huile d'olive": 1,
+  }
+},
+{
+  name: "vinaigre",
+  singleItem: true,
+  ingredients: {
+    "vinaigre": 1,
+  }
+}];
+
+export default recettes;
