1beacf7badb794c7ce9cd04b93010578c1c2ddf1
Benoit Dekajoo <dekajoo@gmail.com>
2022-03-15 08:31:54 +0100

    yoyo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
 import recettes from './Recettes.js';
+import { categoryMap, categories, ingredientsToValidate } from './Recettes.js';
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [items, setItems] = useState({});
+  const [orderedItems, setOrderedItems] = useState([]);
   const [nameNewItem, setNameNewItem] = useState("");
+  const [showModal, setShowModal] = useState("");
+  const [itemsToValide, setItemsToValide] = useState([]);
+
+
+  useEffect(() => {
+    var itemKeys = Object.keys(items);
+    itemKeys.sort((i1, i2) => categoryMap[i1] < categoryMap[i2] ? 1 : -1)
+    setOrderedItems(itemKeys)
+  }, [items])
+
+
+  useEffect(() => {
+    if (itemsToValide && itemsToValide.length > 0)
+      setShowModal(true);
+    else
+      setShowModal(false);
+  }, [itemsToValide])
 
   function addItems(recetteName) {
     var newItems = Object.assign({}, items);
 
     let recette = recettes.find(x => x.name === recetteName);
+
+    setItemsToValide(Object.keys(recette.ingredients).filter(x => ingredientsToValidate.includes(x)));
 
     for (var item of Object.entries(recette.ingredients)) {
       if (!newItems[item[0]])
@@ -17,7 +38,6 @@ function App() {
       else
         newItems[item[0]] += item[1];
     }
-    console.log("aadd", newItems)
 
     setItems(newItems);
   }
@@ -53,7 +73,8 @@ function App() {
   function copyToClipBoard() {
     var text = "";
     for (var item of Object.entries(items)) {
-      text += item[0] + " x" + item[1] + "\n"
+      if (item[1] > 0)
+        text += item[0] + " x" + item[1] + "\n"
     };
     navigator.clipboard.writeText(text);
   }
@@ -69,6 +90,24 @@ function App() {
     }, 500)
   }
 
+  function getCategoryEmoji(itemKey) {
+    return categories[categoryMap[itemKey]];
+  }
+
+  function removeItem(itemKey) {
+    var newItems = Object.assign({}, items);
+    delete newItems[itemKey]
+    setItems(newItems);
+    var newItemsToValide = itemsToValide.filter(x => x !== itemKey);
+    setItemsToValide(newItemsToValide);
+  }
+
+  function validateItem(itemKey) {
+    var newItemsToValide = itemsToValide.filter(x => x !== itemKey);
+    setItemsToValide(newItemsToValide);
+  }
+
+
   return (
     <div className="App">
       <div className="container">
@@ -82,6 +121,8 @@ function App() {
         <div className="recettes">
           {recettes.map((recette, i) => <div key={i} className={"recette" + (recette.singleItem ? " singleItem" : "")} onClick={() => addItems(recette.name)}>
             <div className="recette-title">{recette.name}</div>
+            <div className="recette-nb-personnes">{recette.personnes ? recette.personnes + "🙋‍♂️" : ""}</div>
+            <div className="recette-link">{recette.link ? (<a href={recette.link} target="_blank" onClick={(event) => event.stopPropagation()}>🔗</a>) : null}</div>
           </div>)}
         </div>
 
@@ -89,11 +130,15 @@ function App() {
           Courses
         </div>
         <div className="list">
-          {Object.entries(items).map((item, i) => <div key={i} className="item">
-            <span className="itemName">{item[0]}</span>
-            <span className="itemButton" onClick={() => decreaseItem(item[0])}>⬅</span>
-            <span className="itemQuantity">{item[1]}</span>
-            <span className="itemButton" onClick={() => increaseItem(item[0])}>➡</span></div>)}
+          {orderedItems.map((itemKey, i) => <div key={i} className={"item " + (items[itemKey] <= 0 ? "noItem" : "")}>
+            <div className="itemName">{getCategoryEmoji(itemKey)} {itemKey}</div>
+            <div className="itemQuantityControls">
+              <div className={"itemButton " + (items[itemKey] <= 0 ? "noItem" : "")} onClick={() => decreaseItem(itemKey)}>⬅</div>
+              <div className="itemQuantity">{items[itemKey]}</div>
+              <div className="itemButton" onClick={() => increaseItem(itemKey)}>➡</div>
+            </div>
+          </div>)}
+
           <div className="item">
             <input type="text" className="itemNameField" onChange={e => setNameNewItem(e.target.value)} value={nameNewItem}></input>
             <button className="actionButton" disabled={!nameNewItem} onClick={() => createNewItem()}>Add</button>
@@ -106,7 +151,17 @@ function App() {
           <button className="actionButton" onClick={() => exportToGKeep()}>export to google keep</button>
         </div>
       </div>
-    </div>
+      {showModal ? <div className="modal-container" onClick={(event) => event.stopPropagation()}>
+        <div className="modal">
+          <div className="modal-title">Déjà du {itemsToValide[0]} en stock ?</div>
+          <div className="modal-actions">
+            <button className="actionButton" onClick={() => removeItem(itemsToValide[0])}>Oui 👍</button>
+            <button className="actionButton" onClick={() => validateItem(itemsToValide[0])}>Non 🛒</button>
+          </div>
+
+        </div>
+      </div> : null}
+    </div >
   );
 }
 
--- src/Recettes.js
+++ src/Recettes.js
@@ -4,36 +4,114 @@ let recettes = [{
     "tomate": 7,
     "pâte feuilletée": 1,
     "pignons de pain": 1
-  }
+  },
+  link: "https://dekajoo.notion.site/Tarte-tatin-tomates-44bca60e13474c1ca08789eef22ac632"
 },
 {
   name: "Stew",
   ingredients: {
-    "oignon": 1,
+    "oignons": 1,
     "ail": 5,
     "gingembre": 1,
     "curcuma": 1,
-    "pois chiches": 2,
-    "Lait de coco gras 400ml": 1,
+    "pois chiches 300g": 2,
+    "lait de coco gras 400ml": 1,
     "bouillons cube": 1,
-    "epinards frais": 1,
-    "pot yaourt Skir": 1,
+    "épinards frais": 1,
+    "pot yaourt skir": 1,
     "tomate": 2,
-    "echalottes": 2
-  }
+    "échalottes": 2
+  },
+  link: "https://owiowifouettemoi.com/2019/01/23/the-stew-pois-chiche-coco-et-curcuma/"
 },
 {
   name: "Burger végé féta",
   ingredients: {
     "féta": 1,
     "oeuf": 1,
-    "farine": 1,
+    "farine 100g": 1,
     "graines": 1,
     "cumin": 1,
     "pain à burger": 2,
     "cornichons": 1,
     "ketchup": 1
-  }
+  },
+  link: "https://dekajoo.notion.site/Steak-feta-cfe2ad9e8ccf4795b9d9da830bc61b0a"
+},
+{
+  name: "Conchiglionis farcis, brousse-épinard",
+  ingredients: {
+    "conchiglionis 300g": 1,
+    "épinard surgelé 500g": 1,
+    "cottage cheese 250g": 1,
+    "concassé tomate 50cl": 1,
+    "chapelure": 1,
+    "noix de muscade": 1,
+  },
+  link: "https://dekajoo.notion.site/Conchiglionis-farcis-brousse-pinard-c8e5fbbca7384fc99b149f1c24cd6036"
+},
+{
+  name: "Pâtes aux oignons rouges & gorgonzola",
+  ingredients: {
+    "pates courtes 250g": 1,
+    "oignons rouges": 3,
+    "gorgonzola 100g": 1,
+    "noix 70g": 1
+  },
+  link: "https://owiowifouettemoi.com/2019/05/21/farniente-pasta/",
+  personnes: 2
+},
+{
+  name: "Pierogi (entrée)",
+  ingredients: {
+    "farine 100g": 3,
+    "oeuf": 3,
+    "cottage cheese 250g": 1,
+    "beurre 10g": 3
+  },
+  link: "https://dekajoo.notion.site/Pierogi-54ec793bc45846808bf84f1e5968573d",
+  personnes: 4
+},
+{
+  name: "Chili végé",
+  ingredients: {
+    "oignons": 3,
+    "carottes": 3,
+    "poivrons": 3,
+    "haricots rouge 300g": 2,
+    "pois chiches 300g": 1,
+    "épices chili": 1,
+    "cumin": 1,
+    "bouillons cube": 1,
+    "concassé tomate 50cl": 2,
+    "chips tortillas": 1
+  },
+  link: "https://dekajoo.notion.site/Chili-v-g-d9e25dc8dda84df39f841c252434b6d6",
+  personnes: 6
+},
+{
+  name: "Lasagne aux épinards",
+  ingredients: {}
+},
+{
+  name: "Lasagne aux légumes du soleil",
+  ingredients: {}
+},
+{
+  name: "Dhal de choux-fleur et de lentille corail",
+  ingredients: {}
+},
+{
+  name: "Curry sri-lankais",
+  ingredients: {}
+},
+{
+  name: "Salade de lentille",
+  ingredients: {}
+},
+{
+  name: "Gateau légé à la compote",
+  ingredients: {}
 },
 {
   name: "PQ",
@@ -69,6 +147,120 @@ let recettes = [{
   ingredients: {
     "vinaigre": 1,
   }
+},
+{
+  name: "sel",
+  singleItem: true,
+  ingredients: {
+    "sel": 1,
+  }
+},
+{
+  name: "poivre",
+  singleItem: true,
+  ingredients: {
+    "poivre": 1,
+  }
+},
+{
+  name: "fruits",
+  singleItem: true,
+  ingredients: {
+    "fruits": 1,
+  }
 }];
+
+export let categories = {
+  "fruits-et-légumes": "🍆",
+  "épices": "🌶️",
+  "laitages": "🐄",
+  "conserves+sauces": "🥫",
+  "hygiène": "🧻",
+  "surgelés": "🧊",
+  "apéro": "🍷"
+}
+
+export let ingredientsToValidate = [
+  "curcuma",
+  "cumin",
+  "bouillons cube",
+  "noix de muscade",
+  "sel",
+  "poivre",
+  "épices chili",
+  "ail",
+  "oignons",
+  "oeuf",
+  "farine 100g"
+]
+
+export let categoryMap = {
+  // "fruits-et-légumes",
+  "oignons": "fruits-et-légumes",
+  "oignons rouges": "fruits-et-légumes",
+  "ail": "fruits-et-légumes",
+  "gingembre": "fruits-et-légumes",
+  "épinards frais": "fruits-et-légumes",
+  "tomate": "fruits-et-légumes",
+  "échalottes": "fruits-et-légumes",
+  "fruits": "fruits-et-légumes",
+  "carottes": "fruits-et-légumes",
+  "poivrons": "fruits-et-légumes",
+
+  // épices
+  "curcuma": "épices",
+  "cumin": "épices",
+  "bouillons cube": "épices",
+  "noix de muscade": "épices",
+  "sel": "épices",
+  "poivre": "épices",
+  "épices chili": "épices",
+
+  // laitages
+  "pot yaourt skir": "laitages",
+  "féta": "laitages",
+  "cottage cheese 250g": "laitages",
+  "gorgonzola 100g": "laitages",
+  "beurre 10g": "laitages",
+  "oeuf": "laitages",
+
+  // Conserves + sauces
+  "cornichons": "conserves+sauces",
+  "ketchup": "conserves+sauces",
+  "pois chiches 300g": "conserves+sauces",
+  "mais": "conserves+sauces",
+  "huile d'olive": "conserves+sauces",
+  "vinaigre": "conserves+sauces",
+  "conchiglionis 300g": "conserves+sauces",
+  "concassé tomate 50cl": "conserves+sauces",
+  "haricots rouge 300g": "conserves+sauces",
+  "pates courtes 250g": "conserves+sauces",
+
+
+
+  // hygiène produit ménager
+  "PQ": "hygiène",
+  "dentrifice": "hygiène",
+
+  // Surgelés
+  "épinard surgelé 500g": "surgelés",
+
+  // Apéro + boissons
+  "chips tortillas": "apéro",
+
+  //
+
+  "lait de coco gras 400ml": '',
+  "pâte feuilletée": '',
+
+  "pignons de pain": '',
+  "noix 70g": '',
+
+  "farine 100g": '',
+  "graines": '',
+  "pain à burger": '',
+  "café": '',
+  "chapelure": '',
+}
 
 export default recettes;
