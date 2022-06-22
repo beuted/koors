99756f0d53d86d9421ebf433f4ea5de52a85dfe3
b.jehanno <b.jehanno@criteo.com>
2022-06-22 09:13:56 +0200

    Koors V1.0.0

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import './Login.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import TextField from '@mui/material/TextField';
 
@@ -8,11 +8,13 @@ function Login() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [registerMode, setRegisterMode] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+  const [registerError, setRegisterError] = useState(false);
 
   const navigate = useNavigate();
 
   async function loginToTheApp() {
-    if (!user || user == '' || !password || password == '')
+    if (!user || user === '' || !password || password === '')
       return;
 
     const hashedPassword = await hashPassword(password);
@@ -30,12 +32,15 @@ function Login() {
       });
       if (!responseJson.ok) {
         console.error("failed to login", await responseJson.text());
+        setLoginError(true);
         return;
       }
     } catch (err) {
       console.error("failed to login", err);
+      setLoginError(true);
       return;
     }
+    setLoginError(false);
 
     localStorage.setItem('auth', hashedPassword);
 
@@ -51,59 +56,71 @@ function Login() {
 
     // Call a endpoint that will create a new entry in db with this login and hashed password and prefill it with info then call "loginToTheApp"
     try {
-    const responseJson = await fetch("/api/register", {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'auth': hashedPassword,
-        'user': user
-      },
-      method: "POST"
-    });
-    if (!responseJson.ok) {
-      console.error("failed to register", await responseJson.text());
+      const responseJson = await fetch("/api/register", {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'auth': hashedPassword,
+          'user': user
+        },
+        method: "POST"
+      });
+      if (!responseJson.ok) {
+        console.error("failed to register", await responseJson.text());
+        setRegisterError(true);
+        return;
+      }
+    } catch (err) {
+      console.error("failed to register", err);
+      setRegisterError(true);
       return;
     }
-  } catch (err) {
-    console.error("failed to register", err);
-    return;
-  }
+
+    setRegisterError(false);
 
     loginToTheApp();
   }
 
   return (
     <div className="Login">
+      <div className="title">
+        Koors
+      </div>
       <div className="login-box">
         <TextField
-          label="Login"
+          label="Utilisateur"
           type="string"
           variant="standard"
           onChange={(event) => { setUser(event.target.value) }}
+          error={loginError || registerError}
+          helperText={loginError ? "Utilisateur ou Mot de passe invalide" : (registerError ? "L'utilisateur existe déjà" : null)}
+          sx={{ mb: 2 }}
         />
         <TextField
-          label="Password"
+          label="Mot de passe"
           variant="standard"
           type="password"
           onChange={(event) => { setPassword(event.target.value) }}
+          sx={{ mb: 2 }}
         />
 
         { registerMode ? <TextField
-          label="Confirm Password"
+          label="Confirmer le Mot de passe"
           variant="standard"
           type="password"
           onChange={(event) => { setConfirmPassword(event.target.value) }}
+          sx={{ mb: 2 }}
         /> : null }
 
         <div>
         { !registerMode ? <>
-            <button className="actionButton" onClick={() => loginToTheApp()} disabled={!user || user == '' || !password || password == ''}>Login</button>
-            <button className="actionButton light" onClick={() => setRegisterMode(true)}>Register</button>
+            <button className="actionButton" onClick={() => loginToTheApp()} disabled={!user || user === '' || !password || password === ''}>Login</button>
+            <button className="actionButton light" onClick={() => {setRegisterMode(true); setLoginError(false);}}>Register</button>
           </>
           : 
           <>
-            <button className="actionButton" onClick={() => registerToTheApp()} disabled={!user || user == '' || !password || password == '' || confirmPassword != password}>New account</button>
-            <button className="actionButton light" onClick={() => setRegisterMode(false)}>Back</button>
+            <button className="actionButton" onClick={() => registerToTheApp()} disabled={!user || user === '' || !password || password === '' || confirmPassword !== password}>New account</button>
+            <button className="actionButton light" onClick={() => {setRegisterMode(false); setRegisterError(false);}}>Back</button>
           </>
         }
         </div>
